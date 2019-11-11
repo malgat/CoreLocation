724a418b40701a1290863bebbe6262c1cfb4da2a
D7703_16 <d7703_16@d7703-16ui-imac.local>
2019-11-11 14:25:08 +0900

    Initial Commit

---
+++ CoreLocation Framework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreLocation Framework
+//
+//  Created by D7703_16 on 2019. 11. 11..
+//  Copyright © 2019년 201720632. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
